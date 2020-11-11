a74bac797eeedc3f7ecab83eec546ca74410336a
Laetitia B <bourgois.laetitia@gmail.com>
2020-11-11 16:56:02 +0100

    first commit

---
+++ src/Catalog/Api/Controllers/CatalogsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogsController : ControllerBase
+    {
+        
+        private readonly ILogger<CatalogsController> _logger;
+
+        public CatalogsController(ILogger<CatalogsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello! Api started!!!";
+        }
+    }
+}
