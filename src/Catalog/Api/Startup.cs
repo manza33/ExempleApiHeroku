f2e9f412b2c9e3cb409d4f7e979360419923916f
Laetitia B <bourgois.laetitia@gmail.com>
2020-11-13 09:56:30 +0100

    Use  github secret for Connectingstring

--- src/Catalog/Api/Startup.cs
+++ src/Catalog/Api/Startup.cs
@@ -15,12 +15,30 @@ namespace Catalog.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment webHostEnvironment)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+            WebHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string GetHerokuConnectionString(string connectionString)
+        {
+            string connectionUrl = WebHostEnvironment.IsDevelopment()
+                ? Configuration["ConnectionStrings:" + connectionString]
+                : Environment.GetEnvironmentVariable(connectionString);
+
+            var databaseUri = new Uri(connectionUrl);
+
+            string db = databaseUri.LocalPath.TrimStart('/');
+            string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+            return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -29,7 +47,7 @@ namespace Catalog.Api
 
             // Définition de l'injection (ICatalog correspond a CatalogRepo)
             services.AddTransient<ICatalogRepository>(service => new CatalogRepository(
-                Configuration.GetConnectionString("ExempleApiHeroku")                
+                Configuration.GetConnectionString(GetHerokuConnectionString("CONNECTION_STRING"))                
             ));
 
             services.AddSwaggerGen(
